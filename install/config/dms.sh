6b6bc53e9ee378f5020ac438236f724001c0fac1
schnipseljagd <joscha@schnipseljagd.org>
2026-02-13 14:59:12 +0100

    set dms wallpaper and profile image

--- install/config/dms.sh
+++ install/config/dms.sh
@@ -1,2 +1,6 @@
 # niri has native systemd session integration. Bind DMS to niri's service
 systemctl --user add-wants niri.service dms
+
+# configure dms
+dms ipc call profile setImage ~/.local/share/momo/assets/logos/momo-logo-small.jpg
+dms ipc call wallpaper set ~/.local/share/momo/assets/wallpapers/momo-olive-grove-wallpaper.png
